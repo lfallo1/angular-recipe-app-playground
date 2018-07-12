498bc9b8e9d34757cd47902dd9b4393f2e391346
Lance Fallon <fallon.lance@gmail.com>
2018-07-12 19:38:36 -0400

    ngSwitch

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -9,6 +9,11 @@ export class AppComponent {
   title = 'App Demo Title';
   mode = 'recipe';
   hideMe: boolean = false;
+  mod: number = 0;
+
+  generateRandomNumber(): void{
+    this.mod = Math.floor(Math.random() * 10000) % 5;
+  }
 
   modeChanged(mode){
     this.mode = mode;
